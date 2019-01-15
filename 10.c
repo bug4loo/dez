dd7e5214ea3e1adb317fe13c2f34bf8052f05af8
bugaloo <bugaloo@caos.anark>
2019-01-14 22:21:22 -0300

    Testando

---
+++ 10.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+int a,b,c,d,e,f,g,h,i,j;
+
+for (a=0 ; a < 10 ; a++) { 
+for (b=0 ; b < 10 ; b++) {
+for (c=0 ; c < 10 ; c++) {
+for (d=0 ; d < 10 ; d++) {
+for (e=0 ; e < 10 ; e++) {
+for (f=0 ; f < 10 ; f++) {
+for (g=0 ; g < 10 ; g++) {
+for (h=0 ; h < 10 ; h++) { 
+for (i=0 ; i < 10 ; i++) { 
+for (j=0 ; j < 10 ; j++) { 
+
+	printf("%d%d%d%d%d%d%d%d%d%d\n", a,b,c,d,e,f,g,h,i,j);
+}}}}}}}}}}
+
+
+return 1;
+
+}
